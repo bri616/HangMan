cd2a7c77b51cff8a1950d8221baccc7bceb512d4
Bri Dotson <bridotson@gmail.com>
2014-09-11 14:38:17 -0700

    Took some game flow out of the class and put it in run function.

--- HangMan.rb
+++ HangMan.rb
@@ -1,56 +1,62 @@
 class HangMan
   require 'colorize'
   require 'set'
-  attr_accessor :secret_word, :letters_tried, :screen, :incorrect_letters, :correct_letters, :exit_message
+  attr_accessor :letters_tried, :screen, :incorrect_letters, :correct_letters, :exit_message, :screen_message
   def initialize
     @letters_tried = []
     @incorrect_letters = []
     @correct_letters = []
     @secret_word = choose_random_word
     @screen = Screen.new()
-    @exit_message = exit_message
   end
 
   def choose_random_word
-    word_array = ["chinook", "cascade", "columbus"]
-    word_array[rand(word_array.length)]
+    word_array.sample
   end
 
-  def ask_for_letter
-    print "Choose a new letter: "
-    new_letter = gets.chomp.downcase
-    alphabet = ("a".."z").to_a
-    if alphabet.include? new_letter
-      check_for_letter(new_letter)
+  def word_array
+    ["chinook", "cascade", "columbus", "horizon", "citra", "mosaic"]
+  end
+
+  def check_guess(letter)
+    check_word_for_letter(letter) if in_alphabet? letter
+  end
+
+  def in_alphabet?(letter)
+    if !("a".."z").to_a.include? letter
+      @screen_message = "Not allowed!".magenta
+      false
     else
-      redraw_hangman
-      puts "Not allowed.".magenta
-      ask_for_letter
+      @screen_message = ""
+      true
     end
   end
 
-  def check_for_letter(new_letter)
-    if !@letters_tried.include? new_letter
-      add_letter(new_letter)
-    else
-      redraw_hangman
-      puts "Already tried that one.".magenta
-      ask_for_letter
-    end
+  def check_word_for_letter(new_letter)
+    add_letter(new_letter) if !@letters_tried.include? new_letter
   end
 
   def add_letter(new_letter)
     letters_tried << new_letter
-    if correct_letter?(new_letter)
-      @correct_letters << new_letter
-    else
-      @incorrect_letters << new_letter
-      @screen.redraw(@incorrect_letters.count-1)
-    end
+    add_to_correct_letters(new_letter) if correct_letter?(new_letter)
+    add_to_incorrect_letters(new_letter) if !correct_letter?(new_letter)
+  end
+
+  def add_to_correct_letters(new_letter)
+    @correct_letters << new_letter
+  end
+
+  def add_to_incorrect_letters(new_letter)
+    @incorrect_letters << new_letter
+    @screen.redraw(error_number)
   end
 
   def correct_letter?(new_letter)
     @secret_word.chars.include? new_letter
+  end
+
+  def error_number
+    @incorrect_letters.count-1
   end
 
   def incorrect_letters_formatted
@@ -78,123 +84,119 @@ class HangMan
   def redraw_hangman
     puts "\e[H\e[2J"
     @screen.display
+    puts @screen_message
     puts incorrect_letters_formatted
     puts correct_letters_formatted
   end
 
   def over?
     if @correct_letters.to_set == @secret_word.chars.to_set
-      puts "You won!"
-      @exit_message = "You won!"
+      self.exit_message = "You won!"
       true
     elsif @incorrect_letters.length == @screen.error_bank.length
-      @exit_message = "You lost! :("
+      self.exit_message = "You lost! :("
       true
     end
+  end
+end
 
+class Screen
+  attr_accessor :picture
+
+  def initialize
+    @picture = blank_screen
   end
 
+  def redraw(error_number)
+      error_row = error_bank[error_number][:row]
+      error_col = error_bank[error_number][:col]
+      error_str = error_bank[error_number][:str]
+      @picture[error_row][error_col ] = randomly_colorize(error_str)
+  end
 
-  class Screen
-    attr_accessor :picture
+  def randomly_colorize(input_str)
+    input_str.colorize(String.colors.sample)
+  end
 
-    def initialize
-      @picture = blank_screen
+  def display
+    @picture.each do |*,line|
+      puts line
     end
+  end
 
-    def redraw(error_number)
-        error_row = error_bank[error_number][:row]
-        error_col = error_bank[error_number][:col]
-        error_str = error_bank[error_number][:str]
-        @picture[error_row][error_col ] = randomly_colorize(error_str)
-    end
+  def blank_screen
+      {
+      a: "  |             ",
+      b: "  |--------|    ",
+      c: "  |             ",
+      d: "  |             ",
+      e: "  |             ",
+      f: "  |             ",
+      g: "  |     _______ ",
+      h: "__|____________ ",
+      j: "                "
+      }
+  end
 
-    def randomly_colorize(input_str)
-      input_str.colorize(String.colors.shuffle.first)
-    end
-
-    def display
-      @picture.each do |*,line|
-        puts line
-      end
-    end
-
-    def blank_screen
-        {
-        a: "  |             ",
-        b: "  |--------|    ",
-        c: "  |             ",
-        d: "  |             ",
-        e: "  |             ",
-        f: "  |             ",
-        g: "  |     _______ ",
-        h: "__|____________ ",
-        j: "                ",
-        k: "                "
-        }
-    end
-
-    def error_bank
-      [
-        {
-          id: :head,
-          row: :c,
-          col: 11,
-          str: "O"
-        },
-        {
-          id: :arm1,
-          row: :d,
-          col: 12,
-          str: "-"
-        },
-        {
-          id:  :body1,
-          row: :d,
-          col: 11,
-          str: "|"
-        },
-        {
-          id: :arm2,
-          row: :d,
-          col: 10,
-          str: "-"
-        },
-        {
-          id: :body2,
-          row: :e,
-          col: 11,
-          str: "|"
-        },
-        {
-          id: :leg1,
-          row: :f,
-          col: 12,
-          str: "\\"
-        },
-        {
-          id: :leg2,
-          row: :f,
-          col: 10,
-          str: "/"
-        },
-      ]
-    end
+  def error_bank
+    [
+      {
+        id: :head,
+        row: :c,
+        col: 11,
+        str: "O"
+      },
+      {
+        id: :arm1,
+        row: :d,
+        col: 12,
+        str: "-"
+      },
+      {
+        id:  :body1,
+        row: :d,
+        col: 11,
+        str: "|"
+      },
+      {
+        id: :arm2,
+        row: :d,
+        col: 10,
+        str: "-"
+      },
+      {
+        id: :body2,
+        row: :e,
+        col: 11,
+        str: "|"
+      },
+      {
+        id: :leg1,
+        row: :f,
+        col: 12,
+        str: "\\"
+      },
+      {
+        id: :leg2,
+        row: :f,
+        col: 10,
+        str: "/"
+      },
+    ]
   end
 end
 
 def run
   hangman_game = HangMan.new
-  while true
+  until hangman_game.over?
     hangman_game.redraw_hangman
-    hangman_game.ask_for_letter
-    if hangman_game.over?
-      hangman_game.redraw_hangman
-      puts "Game Over."
-      puts hangman_game.exit_message
-      break
-    end
+    print "Guess a letter: "
+    letter = gets.chomp.downcase
+    hangman_game.check_guess(letter)
   end
+  hangman_game.redraw_hangman
+  puts "Game Over."
+  puts hangman_game.exit_message
 end
 
 run
